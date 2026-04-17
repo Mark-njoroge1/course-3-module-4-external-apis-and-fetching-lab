541902ad93bdd1c617b60532b9d9733f27d5cd9a
Mark Njoroge <marknjoroge591@gmail.com>
2026-04-17 18:28:00 +0300

    add index.js file

--- index.js
+++ index.js
@@ -1,4 +1,61 @@
 // index.js
 const weatherApi = "https://api.weather.gov/alerts/active?area="
 
-// Your code here!
+async function fetchWeatherData(state) {
+    const url = `https://api.weather.gov/alerts/active?area=${state}`;
+    
+    try {
+        const response = await fetch(url);
+        if (!response.ok) {
+            throw new Error("Failed to fetch weather data");
+        }
+
+        const data = await response.json();
+        displayWeather(data, state);
+    } catch (error) {
+        displayError("network failure");
+    }
+}
+
+function displayWeather(data, state) {
+    document.getElementById('error-message').classList.add('hidden');
+    const alertDisplay = document.getElementById('alerts-display');
+    const count = data.features.length;
+    
+    alertDisplay.innerHTML = '';
+
+    const title = document.createElement('h2');
+    title.textContent = `Weather Alerts: ${count}`;
+    alertDisplay.appendChild(title);
+
+    data.features.forEach(alert => {
+        const p = document.createElement('p');
+        p.textContent = alert.properties.headline;
+        alertDisplay.appendChild(p);
+    });
+}
+
+function displayError(message) {
+    const errorContainer = document.getElementById('error-message'); 
+    const alertDisplay = document.getElementById('alerts-display');
+    
+    alertDisplay.innerHTML = '';
+    errorContainer.textContent = message;
+    errorContainer.classList.remove('hidden');
+}
+
+const searchButton= document.getElementById('fetch-alerts');
+const stateInput = document.getElementById('state-input');
+
+searchButton.addEventListener('click', () => {
+    const state = stateInput.value.trim().toUpperCase();
+    stateInput.value = '';
+    
+    if (state.length !== 2) {
+        displayError("Please enter a valid 2-letter state abbreviation.");
+        return;
+    }
+
+    fetchWeatherData(state);
+    stateInput.value = ''; 
+});
